f76819dc2bf924bee626f31a0d9a94f360795ca7
elijah johnston <eli@millinno.com>
2019-10-22 21:46:21 +0800

    svelte init

---
+++ ctrlwaste/src/routes/_article.js
@@ -0,0 +1,28 @@
+const article = [
+  {
+    title: "Section 1: Fuck the government",
+    body: "Lorem Ipsum",
+    imgOne: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg",
+    imgTwo: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg"
+  },
+  {
+    title: "Section 2: Fuck the police",
+    body: "Lorem Ipsum",
+    imgOne: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg",
+    imgTwo: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg"
+  },
+  {
+    title: "Section 3: Fuck the people",
+    body: "Lorem Ipsum",
+    imgOne: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg",
+    imgTwo: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg"
+  },
+  {
+    title: "Section 4: Fuck the community",
+    body: "Lorem Ipsum",
+    imgOne: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg",
+    imgTwo: "https://cdn2.lamag.com/wp-content/uploads/sites/6/2019/10/plastic-roads-los-angeles-syed-mahabubul-kader-eyeem-getty-1068x712.jpg"
+  }
+]
+
+export default article;
